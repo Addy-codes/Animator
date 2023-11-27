1d5fa179b8c500fa615aa0f28063d8c0d1aa97bb
Mohd Adeeb Siddiqui <72205091+Addy-codes@users.noreply.github.com>
2023-11-27 13:36:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -47,7 +47,7 @@ def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
-@app.route('/')
+@app.route('/modelgen')
 def index():
     return render_template('index.html')
 
